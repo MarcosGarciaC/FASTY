1b9574df1a1ed89065b32fd07ac25da24a0c0f95
MarcosGarciaC <marcos.garciacastillo459@gmail.com>
2025-06-05 20:15:15 -0600

    g

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://mimamameama:mimamameamamucho@fastysserver.3roekkm.mongodb.net/fasty').then(()=>console.log("Db connected"));
-}
+  const mongoUri = process.env.MONGO_URI;
+
+  if (!mongoUri) {
+    console.error("MongoDB URI not found in environment variables.");
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("DB connected");
+  } catch (error) {
+    console.error("DB connection error:", error);
+    process.exit(1);
+  }
+};
