20c4aad2a4bb73777888221cdc6a61a873415bad
MarcosGarciaC <marcos.garciacastillo459@gmail.com>
2025-06-12 23:36:35 -0600

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addFood,listFood,removeFood, listFoodByCafeteriaId} from '../controllers/foodController.js'
+import { addFood,listFood,removeFood, listFoodByCafeteriaId, updateFood} from '../controllers/foodController.js'
 import multer from 'multer'
 
 const foodRouter = express.Router();
