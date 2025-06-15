d94369bff44369f298936cc68d5405ea5600b519
MarcosGarciaC <marcos.garciacastillo459@gmail.com>
2025-06-15 01:29:02 -0600

    Update foodController.js

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -52,6 +52,17 @@ const addFood = async (req, res) => {
   }
 };
 
+// all food list
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods })
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" })
+  }
+}
+
 // remove food item
 const removeFood = async (req, res) => {
   try {
