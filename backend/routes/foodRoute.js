dd660795c434c8e6cf63a15ad8065c911937cb00
MarcosGarciaC <marcos.garciacastillo459@gmail.com>
2025-06-15 22:37:16 -0600

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,22 +1,14 @@
-import express from 'express'
-import { addFood,listFood,removeFood, listFoodByCafeteriaId, updateFood} from '../controllers/foodController.js'
-import multer from 'multer'
+import express from 'express';
+import { addFood, listFood, removeFood, listFoodByCafeteriaId, updateFood } from '../controllers/foodController.js';
+import multer from 'multer';
+
+// Use memory storage instead of disk storage
+const upload = multer({ storage: multer.memoryStorage() });
 
 const foodRouter = express.Router();
 
-// Image Storage Engine
-
-const storage = multer.diskStorage({
-  destination: "uploads",
-  filename:(req, file, cb)=>{
-    return cb(null,`${Date.now()}${file.originalname}`)
-  }
-})
-
-const upload = multer({storage:storage})
-
-foodRouter.post("/add", upload.single("image") ,addFood)
-foodRouter.get("/list",listFood)
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
 foodRouter.post("/remove", removeFood);
 foodRouter.get("/list/by-cafeteria/:cafeteria_id", listFoodByCafeteriaId);
 foodRouter.put("/update", upload.single("image"), updateFood);
