a6dc83485d92dcc2f37c6b8348f4a9839c99df4d
MarcosGarciaC <marcos.garciacastillo459@gmail.com>
2025-06-22 20:44:19 -0600

    Update foodModel.js

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -13,7 +13,7 @@ const foodSchema = new Schema({
   price: { type: Number, required: true, min: 0 },
   category: {
     type: String,
-    enum: ['Almuerzo','Bebida', 'Snack'],
+    enum: ['Almuerzo','Bebida', 'Snack', 'Reposteria'],
     required: true
   },
 
